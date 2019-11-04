bdcec1609efa02c629a89327a127e3ba018abdec
Rovian Vieceli <rovianvieceli@gmail.com>
2019-11-04 09:30:51 -0300

    Restart repository

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-document.body.className = "blue-grey lighten-5";
-document.getElementById('root').className = "container";
-
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
