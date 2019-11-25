84ab8d3e913d3af0d0ff8829f128b9b18be9bddf
Rovian Vieceli <rovianvieceli@gmail.com>
2019-11-25 18:10:27 -0300

    Go bakc

--- src/Components/Error/NotFound/index.js
+++ src/Components/Error/NotFound/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class NotFound404 extends React.Component {
     render () {
-        return <h6 className="mt-3">Página não encontrada</h6>;
+        return (
+            <div>
+                <h6 className="mt-3">Página não encontrada</h6>
+                <small><Link to="/">Go Back</Link></small>
+            </div>
+        )
     }
 }
